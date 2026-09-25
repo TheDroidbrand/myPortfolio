ae69a2853d8da5df510ba1cfda7617224f7204b7
TheDroidbrand <seyi4614@gmail.com>
2026-09-25 22:33:15 +0100

    project section and education update

--- components/footer.tsx
+++ components/footer.tsx
@@ -21,7 +21,7 @@ export default function Footer() {
               <Github className="h-5 w-5" />
             </Link>
             <Link
-              href="https://linkedin.com/in/YourUsername"
+              href="https://linkedin.com/in/seyifunmiadeojo"
               target="_blank"
               rel="noopener noreferrer"
               className="text-muted-foreground hover:text-primary transition-colors"
